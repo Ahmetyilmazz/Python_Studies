54d06930ba5380d81b8271a6e1d7b80242453da0
Ahmet Yılmaz <70190607+Ahmetyilmazz@users.noreply.github.com>
2020-08-25 08:30:03 +0300

    Add files via upload

---
+++ Basit Matematik İslemleri.py
@@ -0,0 +1,22 @@
+# Calisma Programı.9_Calisma 
+# Programmer : Ahmet YILMAZ 
+
+a = 2
+b = 3
+c = 4
+
+c = a + b                # Toplama işlemi.
+print("Sonuc", c)
+
+c = a - b                # Çıkarma İşlemi.
+print("Sonuc", c)
+
+c = a * b                # Çarpma İşlemi.
+print("Sonuc", c)
+
+c = a / b                # Bölme İşlemi.
+print("Sonuc", c)
+
+c = a**b                 # Üs Alma İşlemi. (a üzeri b olaraktan)
+print(" ", c)
+
