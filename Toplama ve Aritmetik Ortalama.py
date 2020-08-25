d14ba7fa50b2ecea5d670c9b475f481778cae8a0
Ahmet Yılmaz <70190607+Ahmetyilmazz@users.noreply.github.com>
2020-08-25 08:31:07 +0300

    Add files via upload

---
+++ Toplama ve Aritmetik Ortalama.py
@@ -0,0 +1,12 @@
+# Toplama_İslemleri_Calismalari
+# Programmer : AHMET YILMAZ
+
+a= str(input ("1. Sayiyi Giriniz : "))    # 'str' yazılırsa sayi yerine harf yazılmalıdır. 
+b= int(input ("2. Sayiyi Giriniz : "))
+c= int(input ("3. Sayiyi Giriniz : "))
+d= int(input ("4. Sayiyi Giriniz : "))
+e= float(input ("5. Sayiyi Giriniz : "))  # 'float' yazıldığı zaman ise ondalıklı ifade edilir.
+
+
+print("Bu tamsayilarin toplamı :",a+b+c+d+e)
+print("Bu tamsayilarin Aritmetik Ortalaması :",(a+b+c+d+e)/5)
