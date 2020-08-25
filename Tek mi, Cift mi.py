061c4e420b214986fde479de2693d9bb4e4d3693
Ahmet Yılmaz <70190607+Ahmetyilmazz@users.noreply.github.com>
2020-08-25 08:32:21 +0300

    Add files via upload

---
+++ Tek mi, Cift mi.py
@@ -0,0 +1,14 @@
+# Sayinin Tek mi, Cift mi olduğunu belirten program.
+# Programmer : Ahmet YILMAZ 
+
+sayi = input("Sayi Giriniz : ")
+sayi = int(sayi)
+
+if sayi % 2 == 0:
+    print("Sayi Cifttir.")
+else:
+    print("Sayi Tektir.")
+
+
+
+
