ab777608dabadef4636f4ed783bdf46864b88204
Ahmet Yılmaz <70190607+Ahmetyilmazz@users.noreply.github.com>
2021-01-27 06:44:31 -0800

    Add files via upload

---
+++ Genel_Bilgilendirme.py
@@ -0,0 +1,54 @@
+
+
+# Programmer : Ahmet YILMAZ 
+
+
+Flowchart 
+
+'''
+ Teorik olarak kavramları biliyor musunuz ? 
+    - String, veri tipleri ya da programlama dili nedir ya da 
+
+Öncelikle syntax kuralını öğrenmeniz lazım. 
+
+Problem belirlendi, çözmeye kalkmadan önce algoritma oluşturmamız lazım. 
+
+SIKI ÇALIŞMA ! 
+
+'''
+
+#print ( "Hello World ! ") 
+
+
+
+''' --- Programlama dili nedir ? ---
+
+
+Tanım : Bir problemin çözümünün bilgisayarda gerçekleşmesini ifade etmek üzere program oluşturmak için kullanılan araçtır.
+
+Amaçlar :
+
+- Makinalara talimat vermek / davranışlarını kontrol etmek. 
+- Algoritmaları göstermeye/ ifade etmeye yarayan bir notasyon.
+- Yüksek seviyeli tasarımları ifade etmek.
+- Çözümleri ve çözüm yollarını test etmek.
+- Programcılar arasında iletişim sağlamak.
+- Bilgisayarlı cihazları kontrol etmek. 
+
+---- Programlama dlllerinin kategorileri ----
+
+Emirsel (imperative)
+    - Merkezi özellikleri değişkenler, atama ifadeleri ve döngülerdir.
+    - Örnek : C, Pascal 
+
+Fonksiyonal (Functional)
+    - Hesaplama yapmanın tmeelinde verilen parametrelere fonksiyonları uygulamaktır.
+    - Örnek : Haskell, LISP, Schmee 
+
+Mantık (Logic)
+    - Kural tabanlı (Kurallar belirli sıralama olmadan verilir.)
+    - Örnek : Prolog
+
+Nesneye yönelik ( Object -orinted)
+    - Veri soyutlama, miras, kapsülleme
+    - Örnek : Java, C++, Python...
